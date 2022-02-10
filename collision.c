d62600c9b1d46bb269673b05c91160a0d5dce28f
Yoan Cucchi <yoancucchi@MBP-de-Yoan.home>
2022-02-10 15:45:09 +0200

    update

--- collision.c
+++ collision.c
@@ -6,7 +6,7 @@
 /*   By: ycucchi <yoan066@yahoo.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 09:09:30 by ycucchi           #+#    #+#             */
-/*   Updated: 2022/02/10 12:49:07 by ycucchi          ###   ########.fr       */
+/*   Updated: 2022/02/10 15:44:50 by ycucchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,16 @@ int	top_y(int *tet)
 	return(y);
 }
 
-int	box_collision(*tet)
+int	box_collision(int *tet, int size)
 {
-	
-	
+	int	x;
+	int	y;
+
+	x = top_x(tet);
+	y = yop_y(tet);
+	if (x >= size || y >= size)
+		return(1);
+	return(0);
 }
 
 int	piece_collision(char **grid, int *tet)
@@ -60,7 +66,10 @@ int	piece_collision(char **grid, int *tet)
 	
 }
 
-int	collision(char **grid)
+int	collision(char **grid, int *tet, int size)
 {
-	
+	if (box_collision(tet, size) != 0 && piece_collision(tet, size) != 0)
+		return(1);
+	else
+		return(0);
 }
